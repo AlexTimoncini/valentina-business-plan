e8318cd9f44cff1e8d13a83f7225493fcc56b54e
Poratile_Alex <>
2024-12-10 11:12:26 +0100

    init db actions

--- js/home.js
+++ js/home.js
@@ -1,1 +1,28 @@
 console.log("HOMEPAGE")
+init()
+async function init() {
+    let events;
+    await getEvents().then(rs=>{
+        events = rs
+    })
+    console.log(events)
+    events.forEach(ev=>{
+        console.log(ev)
+        document.getElementById("eventi").insertAdjacentHTML("beforeend", `
+            <li>${ev.title} - ${new Date(ev.event_date).toLocaleDateString()}</li>
+        `)
+    })
+}
+/*GET EVENTI*/
+async function getEvents() {
+    try {
+        const response = await fetch('./db/eventi.json');
+        if (!response.ok) {
+            throw new Error('Failed to load eventi table');
+        }
+        const manifest = await response.json()
+        return manifest.eventi
+    } catch (error) {
+        return false
+    }
+}
