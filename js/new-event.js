dde3c54ccdadbc1de5c514d18abf87b20220a18e
Poratile_Alex <>
2024-12-10 12:07:20 +0100

    NEW API AUTHENTICATOR -NEW EVENT

--- js/new-event.js
+++ js/new-event.js
@@ -15,14 +15,25 @@ async function newEventExample() {
 
     const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`;
 
+    let GITHUB_TOKEN = ''
     try {
-        // Step 1: Recupera il file JSON e il suo SHA
+        const response = await fetch("./.env")
+        if (!response.ok) {
+            throw new Error(`Errore nel recupero del file: ${response.status}`);
+        }
+        GITHUB_TOKEN = await response.text();
+        GITHUB_TOKEN = atob(GITHUB_TOKEN.substring(0, GITHUB_TOKEN.length - 1))
+    } catch {
+        console.error("Errore in autenticazione: .env non trovato")
+    }
+    try {
         const fileResponse = await fetch(apiUrl, {
             headers: {
-                Authorization: `Bearer ${GITHUB_TOKEN}`,
-                Accept: 'application/vnd.github.v3+json',
+                'Authorization': `Bearer ${GITHUB_TOKEN}`,
+                'Accept': 'application/vnd.github.v3+json',
+                'Content-Type': 'application/json',
             },
-        });
+        })
 
         if (!fileResponse.ok) {
             throw new Error(`Errore nel recupero del file: ${fileResponse.status}`);
@@ -36,10 +47,10 @@ async function newEventExample() {
         console.log('JSON originale:', jsonData);
 
         jsonData.eventi.push({
-            id: 2,
+            id: 3,
             publish_date: "12/10/2024",
             event_date: "12/31/2024",
-            title: "Capodanno"
+            title: "Capodanno2"
         })
         console.log('JSON modificato:', jsonData);
 
