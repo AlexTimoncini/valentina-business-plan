dad0cd542ea7c4d4e370b0f7d4783973e94d81d8
Poratile_Alex <>
2024-12-12 23:53:56 +0100

    stopppof after 5 seconds

--- js/home.js
+++ js/home.js
@@ -1,5 +1,8 @@
 init()
 async function init() {
+    if(document.querySelector("body > div")){
+        document.querySelector("body > div").remove()
+    }
     /*LOGIN MANAGEMENT*/
     if(sessionStorage.getItem("userId") === null){
         sessionStorage.clear()
